469c28a29c23d8a4da56d0cdc14a4690eb688c93
Petro Kushchak <petro.kushchak@Petros-MacBook-Pro.local>
2022-01-07 22:04:02 +0200

    restart stream if ffmpeg stucked, also stop streaming if someone else using homepod

--- src/lib/airplayDevice.ts
+++ src/lib/airplayDevice.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import * as child from 'child_process';
@@ -8,19 +9,100 @@ import { Logger } from 'homebridge';
  */
 
 export class AirPlayDevice {
+  private readonly lastSeenThresholdMs = 2000;
+  private readonly heartbeatTimeout = 3000;
+  private readonly defaultPlayingStreamName: string = 'Streaming with pyatv';
+
   private ffmpeg: child.ChildProcess = null;
   private atvremote: child.ChildProcess = null;
+  private lastSeen: Record<string, number> = {};
+  private heartbeat: NodeJS.Timeout;
 
   constructor(
     private readonly homepodId: string,
     private readonly logger: Logger,
   ) {}
 
-  public play(streamUrl: string, streamName: string) {
-    if (this.isPlaying()) {
-      this.stop();
-    }
+  public getPlayingTitle(callback: (title: string) => void): void {
+    const currentTitleCmd = `atvremote --id ${this.homepodId} title`;
+    child.exec(currentTitleCmd, (_error, stdout, _stderr) => {
+      callback(stdout);
+    });
+  }
 
+  public play(streamUrl: string, streamName: string) {
+    const heartbeat = this.handleHearbeat.bind(this);
+    const heartbeatFailed = () => {
+      //identify readon and restart streaming...
+      this.getPlayingTitle((title) => {
+        this.logger.info(
+          `Received from device: ${this.homepodId} title: ${title}`,
+        );
+        if (title === '') {
+          //need to restart streaming
+          this.startStreaming(
+            streamUrl,
+            streamName,
+            heartbeat,
+            heartbeatFailed,
+          );
+        } else if (title === this.defaultPlayingStreamName) {
+          //not clear what happened
+          this.logger.info('Looks like still playing but no hearbeat...');
+        } else {
+          //device is used to play something else, need to change state to "STOPPED"
+          this.endStreaming(() => {
+            this.logger.info('Streaming finished');
+          });
+        }
+      });
+    };
+
+    if (this.isPlaying()) {
+      this.endStreaming(() => {
+        this.logger.info('Streaming finished');
+        this.startStreaming(streamUrl, streamName, heartbeat, heartbeatFailed);
+      });
+    } else {
+      this.startStreaming(streamUrl, streamName, heartbeat, heartbeatFailed);
+    }
+  }
+
+  /**
+   * Streaming crash monitoring/prevention
+   */
+  private handleHearbeat(
+    heatbeatType: string,
+    heartbeatFailed: () => void,
+  ): void {
+    this.logger.info(`Playing heartbeat, source: ${heatbeatType}`);
+    if (!this.lastSeen[heatbeatType]) {
+      this.lastSeen[heatbeatType] = Date.now();
+      return;
+    }
+    const lastSeenByType = Object.values(this.lastSeen);
+    const nearest = Math.max(...lastSeenByType);
+    const diff = Date.now() - nearest;
+    this.logger.info(
+      `Playing heartbeat, source: ${heatbeatType} nearest: ${nearest} diff: ${diff}`,
+    );
+    if (diff > this.lastSeenThresholdMs) {
+      this.logger.info(
+        `Heartbeat, failed source: ${heatbeatType}, lastSeen: ${nearest} diff: ${diff}`,
+      );
+      heartbeatFailed();
+    } else {
+      this.lastSeen[heatbeatType] = Date.now();
+      this.logger.info(`Playing heartbeat, source: ${heatbeatType} updated`);
+    }
+  }
+
+  private startStreaming(
+    streamUrl: string,
+    streamName: string,
+    heartbeat: (source: string, heartbeatFailed: () => void) => void,
+    heartbeatFailed: () => void,
+  ) {
     // create pipe for the command:
     //  ffmpeg -i ${streamUrl} -f mp3 - | atvremote --id ${this.homepodId} stream_file=-
 
@@ -35,10 +117,13 @@ export class AirPlayDevice {
       { detached: true },
     );
 
-    this.ffmpeg.stdout.pipe(this.atvremote.stdin);
+    this.ffmpeg.stdout.pipe(this.atvremote.stdin).on('error', (error) => {
+      this.logger.info(`ffmpeg pipe error: ${error}`);
+    });
 
     this.ffmpeg.stderr.on('data', (data) => {
       this.logger.info(`ffmpeg error: ${data}`);
+      heartbeat('ffmpeg', heartbeatFailed);
     });
 
     this.ffmpeg.on('exit', (code, signal) => {
@@ -51,11 +136,39 @@ export class AirPlayDevice {
 
     this.atvremote.stderr.on('data', (data) => {
       this.logger.info(`atvremote error: ${data}`);
+      heartbeat('atvremote', heartbeatFailed);
     });
+
+    this.heartbeat = setInterval(() => {
+      heartbeat('heartbeat', heartbeatFailed);
+    }, this.heartbeatTimeout);
 
     this.logger.info(
       `spawn ffmpeg: ${this.ffmpeg.pid}  atvremote: ${this.atvremote.pid}`,
     );
+  }
+
+  private endStreaming(streamingFinished: () => void) {
+    try {
+      this.logger.info(
+        `Killing process: ffmpeg: ${this.ffmpeg.pid}  atvremote: ${this.atvremote.pid}`,
+      );
+      this.ffmpeg.stdout.unpipe();
+      clearInterval(this.heartbeat);
+
+      setTimeout(() => {
+        this.ffmpeg.kill();
+        this.ffmpeg = null;
+        setTimeout(() => {
+          this.atvremote.kill();
+          this.atvremote = null;
+          streamingFinished();
+        }, 3000);
+      }, 2000);
+
+    } catch (err) {
+      this.logger.info(`Error while trying to stop: ${err}`);
+    }
   }
 
   public stop() {
@@ -63,23 +176,10 @@ export class AirPlayDevice {
       this.logger.info('Trying to stop stopped process!');
       return;
     }
-    try {
-      this.logger.info(
-        `Killing process: ffmpeg: ${this.ffmpeg.pid}  atvremote: ${this.atvremote.pid}`,
-      );
-      this.ffmpeg.stdout.unpipe();
-      setTimeout(() => {
-        this.ffmpeg.kill();
-        this.ffmpeg = null;
-      }, 2000);
 
-      setTimeout(() => {
-        this.atvremote.kill();
-        this.atvremote = null;
-      }, 5000);
-    } catch (err) {
-      this.logger.info(`Error while trying to stop: ${err}`);
-    }
+    this.endStreaming(() => {
+      this.logger.info('Streaming finished');
+    });
   }
 
   public isPlaying(): boolean {
