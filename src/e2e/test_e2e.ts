37b17399099e6478e5223a02700a179d135f3daa
Petro Kushchak <petro.kushchak@macbook>
2023-01-20 16:31:14 +0200

    e2e fix

--- src/e2e/test_e2e.ts
+++ src/e2e/test_e2e.ts
@@ -30,7 +30,7 @@ const deviceE2E = async () => {
         'https://www.apple.com/v/apple-music/q/images/shared/og__ckjrh2mu8b2a_image.png',
     );
 
-    await device.playStream('https://online.radiojazz.ua/RadioJazz_Cover', 'E2E', 0);
+    await device.playStream('https://online.radiojazz.ua/RadioJazz_Cover', 'E2E');
     await delay(5000, 0);
     console.log(`IS_PLAYING: ${device.isPlaying()}`);
     await device.stop();
